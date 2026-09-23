2fa535e310b56d7b34419117fdd948bd491ed84b
pos <pgousdal@users.noreply.github.com>
2026-09-23 21:22:50 +0200

    labs: add first Course 08 optimization labs

---
+++ labs/08-6510-optimization/04-tables-precalc/main.asm
@@ -0,0 +1,21 @@
+value=$fb
+*=$0801
+.word e
+.word 10
+.byte $9e
+.text "2064"
+.byte 0
+e:.word 0
+*=$0810
+start:
+ lda value
+ asl
+ asl
+ sta $c000
+ ldx value
+ lda times4,x
+ sta $c001
+ rts
+times4:
+ .byte 0,4,8,12,16,20,24,28
+ .byte 32,36,40,44,48,52,56,60
