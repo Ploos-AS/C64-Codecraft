65100f381a9c98de29379c1a9dc06c27be27bd19
pos <pgousdal@users.noreply.github.com>
2026-09-23 21:35:37 +0200

    labs: add first Course 11 capstone labs

---
+++ labs/11-capstone/05-sync-flow/main.asm
@@ -0,0 +1,25 @@
+tick=$fb
+cue=$fc
+*=$0801
+.word e
+.word 10
+.byte $9e
+.text "2064"
+.byte 0
+e:.word 0
+*=$0810
+start:
+ inc tick
+ lda tick
+ lsr
+ lsr
+ lsr
+ lsr
+ and #3
+ sta cue
+ tax
+ lda cue_colours,x
+ sta $d020
+ rts
+cue_colours:
+ .byte 0,6,14,1
