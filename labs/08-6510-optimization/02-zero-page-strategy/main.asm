7323de79f7d8a77bec7b819282193f478ef974de
pos <pgousdal@users.noreply.github.com>
2026-09-23 21:22:25 +0200

    labs: add first Course 08 optimization labs

---
+++ labs/08-6510-optimization/02-zero-page-strategy/main.asm
@@ -0,0 +1,29 @@
+ptr=$f8
+phase=$fa
+temp=$fb
+*=$0801
+.word e
+.word 10
+.byte $9e
+.text "2064"
+.byte 0
+e:.word 0
+*=$0810
+start:
+ lda #<source
+ sta ptr
+ lda #>source
+ sta ptr+1
+ ldy #0
+copy:
+ lda (ptr),y
+ sta $c000,y
+ iny
+ cpy #8
+ bne copy
+ inc phase
+ lda phase
+ sta temp
+ rts
+source:
+ .byte 8,7,6,5,4,3,2,1
