e5bb35d9e657d1faa4f4400a403a158d5b239a0f
pos <pgousdal@users.noreply.github.com>
2026-09-23 21:08:29 +0200

    labs: complete Course 05 demo architecture labs

---
+++ labs/05-demo-architecture/05-part-state-and-cues/main.asm
@@ -0,0 +1,36 @@
+frame=$fb
+cue=$fc
+part_state=$fd
+*=$0801
+.word e
+.word 10
+.byte $9e
+.text "2064"
+.byte 0
+e:.word 0
+*=$0810
+start:
+ jsr cue_update
+ jsr part_update
+ jsr part_render
+ rts
+cue_update:
+ inc frame
+ lda frame
+ and #$0f
+ bne no_cue
+ inc cue
+no_cue:
+ rts
+part_update:
+ lda cue
+ cmp part_state
+ beq unchanged
+ sta part_state
+unchanged:
+ rts
+part_render:
+ lda part_state
+ and #15
+ sta $d020
+ rts
