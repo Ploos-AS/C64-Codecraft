92ce2a394bdb145019f831a2bd2524730730ad9b
pos <pgousdal@users.noreply.github.com>
2026-09-23 20:54:44 +0200

    labs: complete Course 02 raster timing labs

---
+++ labs/02-raster-timing/06-border-timing-probe/main.asm
@@ -0,0 +1,23 @@
+TARGET=$80
+*=$0801
+.word e
+.word 10
+.byte $9e
+.text "2064"
+.byte 0
+e:.word 0
+*=$0810
+start:
+wait:
+ lda $d012
+ cmp #TARGET
+ bne wait
+ lda #2
+ sta $d020
+ ldx #8
+work:
+ dex
+ bne work
+ lda #0
+ sta $d020
+ rts
