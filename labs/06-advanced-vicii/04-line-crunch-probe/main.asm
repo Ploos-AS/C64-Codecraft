f62948066bfa93220a15314bf306f64f2d336391
pos <pgousdal@users.noreply.github.com>
2026-09-23 21:10:49 +0200

    labs: add first Course 06 advanced VIC-II labs

---
+++ labs/06-advanced-vicii/04-line-crunch-probe/main.asm
@@ -0,0 +1,27 @@
+TARGET=$80
+*=$0801
+.word e
+.word 10
+.byte $9e
+.text "2064"
+.byte 0
+e:.word 0
+*=$0810
+start:
+leave:
+ lda $d012
+ cmp #TARGET
+ beq leave
+wait:
+ lda $d012
+ cmp #TARGET
+ bne wait
+ lda $d011
+ sta $c000
+ lda #2
+ sta $d020
+ nop
+ nop
+ lda #0
+ sta $d020
+ rts
